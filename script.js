7a44d4405fd72c0e36072e6d73607547f7f88c7b
DAN <dan.nemer@gmail.com>
2022-09-01 13:15:18 -0400

    JS start

---
+++ script.js
@@ -0,0 +1,39 @@
+var timerEl = document.getElementById('timer');
+var timeLeft = 90;
+var 
+
+
+
+var questions = [
+    {question: "Where should a script tag be linked in an HTML document?",
+    choices: ["before the body closing tag", "within the header", "after the HTML closing tag", "No need" ]
+    answer: "before the closing body tag"},
+
+    {question: "Where should a script tag be linked in an HTML document?",
+    choices: []
+    answer:      },
+
+    {question: "Where should a script tag be linked in an HTML document?",
+    choices: []
+    answer:      },
+
+    {question: "Where should a script tag be linked in an HTML document?",
+    choices: []
+    answer:      },
+
+    {question: "Where should a script tag be linked in an HTML document?",
+    choices: []
+    answer:      },
+
+    {question: "Where should a script tag be linked in an HTML document?",
+    choices: []
+    answer:      },
+    
+]
+
+
+
+
+function displaymessage() {
+
+}
